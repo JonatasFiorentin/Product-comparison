6a9d6f4858e0bbca8a27e070ee17aead7bc0473a
belloli0 <felipebelloli4@gmail.com>
2024-08-04 23:19:30 -0300

    DescricaoSofa

---
+++ src/components/DescricaoSofa.tsx
@@ -0,0 +1,101 @@
+export function DescricaoSofa() {
+    return (
+        <div className="pt-3">
+            <div className="flex flex-row   items-center border-t-2  ">
+                <div className="flex flex-col gap-28  pl-12 pt-5 items-center text-[18px] border-r-2 pr-12">
+                    <div className="flex flex-col gap-5 ">
+                        <p className="text-3xl font-semibold ">General</p>
+                        <p>Sales Package</p>
+                        <p>Model Number</p>
+                        <p>Secondary Material</p>
+                        <p>Configuration</p>
+                        <p>Upholstery Material</p>
+                        <p>Upholstery Color</p>
+
+
+
+                    </div>
+
+                    <div className="flex flex-col gap-5     ">
+                        <p className="text-3xl font-semibold ">Dimensions</p>
+                        <p>Filling Material</p>
+                        <p>Finish Type</p>
+                        <p>Adjustable Headrest</p>
+                        <p>Maximum Load Capacity</p>
+                        <p>Origin of Manufacture</p>
+                    </div>
+                    <div className="flex flex-col gap-4 pr-24 ">
+                        <p className="text-3xl font-semibold ">Product</p>
+                        <p>Width</p>
+                        <p>Height</p>
+                        <p>Depth</p>
+                        <p>Weight</p>
+                        <p>Seat Height</p>
+                        <p>Leg Height</p>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-28  pl-12 pt-20 items-center text-[18px] border-r-2 pr-12">
+                    <div className="flex flex-col gap-5 ">
+                        <p>1 sectional sofa</p>
+                        <p>TFCBLIGRBL6SRHS</p>
+                        <p>Solid Wood</p>
+                        <p>L-shaped</p>
+                        <p>Fabric + Cotton</p>
+                        <p>Bright Grey & Lion</p>
+
+
+
+                    </div>
+                    <div className="flex flex-col gap-5 pt-12 ">
+                        <p>Foam</p>
+                        <p>Bright Grey & Lion</p>
+                        <p>No</p>
+                        <p>280 KG</p>
+                        <p>India</p>
+                    </div>
+                    <div className="flex flex-col gap-5 pt-12 pr-20 ">
+                        <p>265.32 cm</p>
+                        <p>76 cm</p>
+                        <p>167.76 cm</p>
+                        <p>45 KG</p>
+                        <p>41.52 cm</p>
+                        <p>5.46 cm</p>
+
+                    </div>
+                </div>
+                <div className="flex flex-col gap-28  pl-12 pt-5 items-center text-[18px] border-r-2 pr-12">
+                    <div className="flex flex-col gap-5 pt-12 ">
+                        <p>1 Three Seater, 2 Single <br />
+                            Seater</p>
+                        <p>DTUBLIGRBL568</p>
+                        <p>Solid Wood</p>
+                        <p>L-shaped</p>
+                        <p>Fabric + Cotton</p>
+                        <p>Bright Grey & Lion</p>
+
+
+
+
+                    </div>
+                    <div className="flex flex-col gap-5 pt-6">
+                        <p>Matte</p>
+                        <p>Bright Grey & Lion</p>
+                        <p>yes</p>
+                        <p>300 KG</p>
+                        <p>India</p>
+
+
+                    </div>
+                    <div className="flex flex-col gap-5 pt-12 pr-16">
+                        <p>265.32 cm</p>
+                        <p>76 cm</p>
+                        <p>167.76 cm</p>
+                        <p>65 KG</p>
+                        <p>41.52 cm</p>
+                        <p>5.46 cm</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
